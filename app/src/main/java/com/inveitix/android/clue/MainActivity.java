20674aa8a32d7b026ca3defa1f5af9c8dbf4c626
Krystev <tihomir.krastev@mail.bg>
2015-12-21 16:38:31 +0200

    Fixed pull request comments

--- app/src/main/java/com/inveitix/android/clue/MainActivity.java
+++ app/src/main/java/com/inveitix/android/clue/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.inveitix.android.clue.adapters.RecListAdapter;
 import com.inveitix.android.clue.cmn.Museum;
@@ -15,22 +16,35 @@ import com.inveitix.android.clue.cmn.Museum;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    List<Museum> museums;
+    @Bind(R.id.recView)
+    RecyclerView recView;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    private List<Museum> museums;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         museums = new ArrayList<>();
-        RecyclerView recView = (RecyclerView) findViewById(R.id.recView);
+        museums.add(new Museum("Vratsa", null));
+
+        initViews();
+
+    }
+
+    private void initViews() {
+
+        setSupportActionBar(toolbar);
         recView.setLayoutManager(new LinearLayoutManager(this));
         RecListAdapter adapter = new RecListAdapter(this, museums);
         recView.setAdapter(adapter);
-
-
     }
 
     @Override
@@ -43,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
-
+                Toast.makeText(MainActivity.this, "Searching...", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_settings:
-
+                Toast.makeText(MainActivity.this, "Settings pressed", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 super.onOptionsItemSelected(item);
