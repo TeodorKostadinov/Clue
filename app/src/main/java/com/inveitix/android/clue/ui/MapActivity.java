205058ba771e58c98b96d33c0b68a60627d8140b
TeodorKostadinov <teodor.kk.93@gmail.com>
2015-12-23 12:46:14 +0200

    Drawing a custom view with custom shape

--- app/src/main/java/com/inveitix/android/clue/ui/MapActivity.java
+++ app/src/main/java/com/inveitix/android/clue/ui/MapActivity.java
@@ -3,22 +3,55 @@ package com.inveitix.android.clue.ui;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import com.inveitix.android.clue.R;
+import com.inveitix.android.clue.cmn.Point;
+import com.inveitix.android.clue.ui.views.RoomView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MapActivity extends AppCompatActivity {
 
+    private static final String TAG = "MapActivity";
     @Bind(R.id.grp_map_container)
     ViewGroup grpMapContainer;
+    @Bind(R.id.room)
+    RoomView roomView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        ButterKnife.bind(this);
+        List<Point> roomPoints = new ArrayList<>();
+        generateFakeRoom(roomPoints);
+        roomView.setShape(roomPoints);
+    }
 
-
+    private void generateFakeRoom(List<Point> roomPoints) {
+        roomPoints.add(new Point(0, 0));
+        roomPoints.add(new Point(0.25, 0));
+        roomPoints.add(new Point(0.25, 0.25));
+        roomPoints.add(new Point(0.5, 0.25));
+        roomPoints.add(new Point(0.5, 0.5));
+        roomPoints.add(new Point(0.25, 0.5));
+        roomPoints.add(new Point(0.25, 0.62));
+        roomPoints.add(new Point(0.62, 0.62));
+        roomPoints.add(new Point(0.62, 0.12));
+        roomPoints.add(new Point(0.75, 0.12));
+        roomPoints.add(new Point(0.75, 0.75));
+        roomPoints.add(new Point(1, 0.75));
+        roomPoints.add(new Point(1, 1));
+        roomPoints.add(new Point(0.5, 1));
+        roomPoints.add(new Point(0.5, 0.87));
+        roomPoints.add(new Point(0.25, 0.87));
+        roomPoints.add(new Point(0.25, 1));
+        roomPoints.add(new Point(0, 1));
     }
 }
--- app/src/main/java/com/inveitix/android/clue/ui/views/RoomView.java
+++ app/src/main/java/com/inveitix/android/clue/ui/views/RoomView.java
@@ -3,28 +3,44 @@ package com.inveitix.android.clue.ui.views;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.inveitix.android.clue.cmn.Point;
+
+import java.util.List;
 
 /**
  * Created by fos on 23.12.2015 г..
  */
 public class RoomView extends View {
 
+    private static final String TAG = "RoomView";
     private Paint textPaint;
     private Paint roomPaint;
     private float textHeight = 25;
     private int textColor = 0x000000;
     private int maxHeight;
     private int maxWidth;
+    private List<Point> shape;
 
     public RoomView(Context context) {
         super(context);
+        Log.e(TAG, "Room created");
         init();
     }
 
     public RoomView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Log.e(TAG, "Room created");
+        init();
+    }
+
+    public RoomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        Log.e(TAG, "Room created");
         init();
     }
 
@@ -47,6 +63,12 @@ public class RoomView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         this.maxWidth = w;
         this.maxHeight = h;
+        Log.e(TAG, "View width:" + maxWidth);
+        Log.e(TAG, "View height:" + maxHeight);
+    }
+
+    public void setShape(List<Point> shape) {
+        this.shape = shape;
         invalidate();
         requestLayout();
     }
@@ -55,9 +77,20 @@ public class RoomView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(0, 0, 5, roomPaint);
-        canvas.drawCircle(maxWidth, 0, 5, roomPaint);
-        canvas.drawCircle(0, maxHeight, 5, roomPaint);
-        canvas.drawCircle(maxWidth, maxHeight, 5, roomPaint);
+        canvas.drawCircle(0, 0, 15, roomPaint);
+        canvas.drawCircle(maxWidth, 0, 15, roomPaint);
+        canvas.drawCircle(0, maxHeight, 15, roomPaint);
+        canvas.drawCircle(maxWidth, maxHeight, 15, roomPaint);
+        Path path = new Path();
+        path.reset();
+        if(shape != null) {
+            for(Point p : shape) {
+                path.lineTo(maxWidth * p.x, maxHeight * p.y);
+            }
+            if(shape.size() > 0) {
+                path.lineTo(maxWidth * shape.get(0).x, maxHeight * shape.get(0).y);
+            }
+            canvas.drawPath(path, roomPaint);
+        }
     }
 }
