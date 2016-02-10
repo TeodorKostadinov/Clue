4912c77ae5c34569dae13ace77ea5765412b6311
Krystev <tihomir.krastev@mail.bg>
2016-02-10 14:15:12 +0200

    All fields in create map screen made mandatory

--- app/src/main/java/com/inveitix/android/clue/ui/CreateMapActivity.java
+++ app/src/main/java/com/inveitix/android/clue/ui/CreateMapActivity.java
@@ -3,6 +3,8 @@ package com.inveitix.android.clue.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.inveitix.android.clue.R;
 
@@ -11,6 +13,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CreateMapActivity extends AppCompatActivity {
+    @Bind(R.id.edt_museum_name)
+    EditText edtMuseumName;
+    @Bind(R.id.edt_description)
+    EditText edtDescription;
+    private boolean gotLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +26,24 @@ public class CreateMapActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.btn_get_location)
+    public void getLocation() {
+        gotLocation = true;
+    }
+
     @OnClick(R.id.btn_create_room)
     public void openRoom() {
-        Intent intent = new Intent(CreateMapActivity.this, CreateRoomActivity.class);
-        startActivity(intent);
-
+        String museumName = edtMuseumName.getText().toString();
+        String museumInfo = edtDescription.getText().toString();
+        if (museumName.equalsIgnoreCase("") || museumName.length() < 3) {
+            edtMuseumName.setError(getString(R.string.err_txt_museum_name));
+        } else if (museumInfo.equalsIgnoreCase("") || museumInfo.length() < 10){
+            edtDescription.setError(getString(R.string.err_txt_info));
+        } else if(!gotLocation) {
+            Toast.makeText(CreateMapActivity.this, R.string.err_txt_location, Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(CreateMapActivity.this, CreateRoomActivity.class);
+            startActivity(intent);
+        }
     }
 }
