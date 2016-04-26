58db24e226ee47d54139947f86aada8f90627272
Krystev <tihomir.krastev@mail.bg>
2016-04-26 12:52:02 +0300

    Get location button getting current location

--- app/src/main/java/com/inveitix/android/clue/ui/CreateMapActivity.java
+++ app/src/main/java/com/inveitix/android/clue/ui/CreateMapActivity.java
@@ -1,34 +1,76 @@
 package com.inveitix.android.clue.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.inveitix.android.clue.R;
+import com.inveitix.android.clue.cmn.Museum;
+import com.inveitix.android.clue.database.DBLoader;
+import com.inveitix.android.clue.database.DBUtils;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class CreateMapActivity extends AppCompatActivity {
+public class CreateMapActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    public static final int PERMISSIONS_REQUEST_LOCATION = 0;
+    public static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     @Bind(R.id.edt_museum_name)
     EditText edtMuseumName;
     @Bind(R.id.edt_description)
     EditText edtDescription;
+    @Bind(R.id.txt_location)
+    TextView txtLocation;
     private boolean gotLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double currentLatitude;
+    private double currentLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_map);
         ButterKnife.bind(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @OnClick(R.id.btn_get_location)
     public void getLocation() {
-        gotLocation = true;
+        mGoogleApiClient.connect();
     }
 
     @OnClick(R.id.btn_create_room)
@@ -37,13 +79,90 @@ public class CreateMapActivity extends AppCompatActivity {
         String museumInfo = edtDescription.getText().toString().trim();
         if (museumName.equalsIgnoreCase("") || museumName.length() < 3) {
             edtMuseumName.setError(getString(R.string.err_txt_museum_name));
-        } else if (museumInfo.equalsIgnoreCase("") || museumInfo.length() < 10){
+        } else if (museumInfo.equalsIgnoreCase("") || museumInfo.length() < 10) {
             edtDescription.setError(getString(R.string.err_txt_info));
-        } else if(!gotLocation) {
+        } else if (!gotLocation) {
             Toast.makeText(CreateMapActivity.this, R.string.err_txt_location, Toast.LENGTH_SHORT).show();
         } else {
             Intent intent = new Intent(CreateMapActivity.this, CreateRoomActivity.class);
+            saveMuseum();
+            if (mGoogleApiClient.isConnected()) {
+                LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+                mGoogleApiClient.disconnect();
+            }
             startActivity(intent);
         }
+    }
+
+    private void saveMuseum() {
+        Museum museum = new Museum();
+        museum.setDescription(edtDescription.getText().toString().trim());
+        museum.setLocation(currentLatitude +", " + currentLongitude);
+        museum.setName(edtMuseumName.getText().toString().trim());
+        museum.setMapSizeKB(512);
+        List<Museum> museums = DBLoader.getInstance(this).getMuseums();
+        museum.setId(museums.get(museums.size()-1).getId() + 1);
+        Toast.makeText(CreateMapActivity.this, String.valueOf(museum.getId()), Toast.LENGTH_SHORT).show();
+        //DBUtils.getInstance(this).writeMuseumRecord();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestFineLocationPermission();
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission();
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+            currentLatitude = location.getLatitude();
+            currentLongitude = location.getLongitude();
+            txtLocation.setText(String.valueOf("Lat: " + currentLatitude + ", \nLon: " +currentLongitude));
+            gotLocation = true;
+            Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST_LOCATION);
+    }
+
+    private void requestFineLocationPermission() {
+        ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_FINE_LOCATION);
     }
 }
