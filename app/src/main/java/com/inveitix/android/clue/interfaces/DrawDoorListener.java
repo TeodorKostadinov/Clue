8bd00e7597aa251246ab7a4937f2dc77758c66ea
Krystev <tihomir.krastev@mail.bg>
2016-06-14 11:56:33 +0300

    inner interfaces extracted to other package

---
+++ app/src/main/java/com/inveitix/android/clue/interfaces/DrawDoorListener.java
@@ -0,0 +1,7 @@
+package com.inveitix.android.clue.interfaces;
+
+import com.inveitix.android.clue.cmn.Door;
+
+public interface DrawDoorListener {
+    void onDoorDrawn(Door door);
+}
---
+++ app/src/main/java/com/inveitix/android/clue/interfaces/DrawQrListener.java
@@ -0,0 +1,7 @@
+package com.inveitix.android.clue.interfaces;
+
+import com.inveitix.android.clue.cmn.QR;
+
+public interface DrawQrListener {
+    void onQrDrawn(QR qr);
+}
