f277c9421cbd4d911871df7d084e8677ab089b9b
Lilly Mihaylova <lillymihaylova90@gmail.com>
2017-02-02 14:29:17 +0200

    set app version dynamicly

--- app/src/main/java/com/inveitix/android/clue/ui/AboutActivity.java
+++ app/src/main/java/com/inveitix/android/clue/ui/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.inveitix.android.clue.ui;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -11,16 +12,29 @@ import android.widget.TextView;
 import com.inveitix.android.clue.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class AboutActivity extends AppCompatActivity {
+
+    @Bind(R.id.txt_version)
+    TextView txtVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String value = intent.getStringExtra("Key_About_Activity");
+        String appVersion = null;
+        try {
+            appVersion = this.getPackageManager().getPackageInfo(this.getPackageName(),0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        txtVersion.setText("Version " + appVersion);
 
     }
 }
